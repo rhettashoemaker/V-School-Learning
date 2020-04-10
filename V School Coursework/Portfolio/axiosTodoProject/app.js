8ecb658d15a5401629759db74b82daaabda47635
Rhetta Shoemaker <rhetta.shoemaker@gmail.com>
2020-04-10 12:26:49 -0600

    finished axios!

--- V School Coursework/Portfolio/axiosTodoProject/app.js
+++ V School Coursework/Portfolio/axiosTodoProject/app.js
@@ -1,34 +1,81 @@
-console.log(axios.get("https://api.vschool.io/rhettashoemaker/todo"));
-
-
 const form = document.todoListForm;
-const itemContainer = document.getElementById('todoItemsContainer');
-
-
-
-//GET//
-
-function getData (){
-    axios.get("https://api.vschool.io/rhettashoemaker/todo")
-    .then(res => listData(res.data))
-    .catch(err => console.log(err))
-}
-
-
-
-//SHOW UP//
 
 function clearList() {
-    while(todoItemsContainer.firstChild) {
-        form.removeChild(el,firstChild)
+    const el = document.getElementById('todoItemsContainer')
+    while(el.firstChild) {
+        el.removeChild(el.firstChild)
     }
 }
 
-getData()
+function listData (data){
+    clearList()
 
+    for (let i = 0; i < data.length; i++) {
+        const itemContainer = document.getElementById('todoItemsContainer');
+        const itemTitle = document.createElement('h5');
+        const itemPrice = document.createElement('h6'); 
+        const itemDescription = document.createElement('p');
+        const itemImageURL = document.createElement('p');
+        const itemEditButton = document.createElement('button');
+        const itemDeleteButton = document.createElement('button');
+        const itemCompleteButton = document.createElement('button');
 
+        itemTitle.textContent = data[i].title
+        itemPrice.textContent = data[i].price
+        itemDescription.textContent = data[i].description
+        itemImageURL.textContent = data[i].imgURL
+        itemEditButton.textContent = "Edit"
+        itemDeleteButton.textContent = "Delete"
+        itemCompleteButton.textContent = "Complete" 
 
-//POST//
+        itemEditButton.setAttribute('class', 'editButton')
+        itemDeleteButton.setAttribute('class', 'deleteButton')
+        itemCompleteButton.setAttribute('class', 'completeButton')
+        
+        // itemEditButton.addEventListener('click', function () {
+        //     const updates {
+
+        //     }
+        //     axios.put("https://api.vschool.io/rhettashoemaker/todo/" + todo._id, updates)
+        // })
+        itemDeleteButton.addEventListener('click', function () {
+            axios.delete("https://api.vschool.io/rhettashoemaker/todo/" + data[i]._id)
+                .then(response => getData())
+                .catch(error => console.log(error))
+        })
+        itemCompleteButton.addEventListener('click', function () {
+            const updates = {
+                completed: true
+            }
+            
+            itemTitle.style.textDecoration = "line-through"
+            itemPrice.style.textDecoration = "line-through"
+            itemDescription.style.textDecoration = "line-through"
+            itemImageURL.style.textDecoration = "line-through"
+            itemTitle.style.color = "grey"
+            itemPrice.style.color = "grey"
+            itemDescription.style.color = "grey"
+            itemImageURL.style.color = "grey"
+
+            axios.put("https://api.vschool.io/rhettashoemaker/todo/" + data[i]._id, updates)
+        })
+
+        itemContainer.appendChild(itemTitle);
+        itemContainer.appendChild(itemPrice);
+        itemContainer.appendChild(itemDescription);
+        itemContainer.appendChild(itemImageURL);
+        itemContainer.appendChild(itemEditButton);
+        itemContainer.appendChild(itemDeleteButton);
+        itemContainer.appendChild(itemCompleteButton);
+    }
+}
+
+function getData() {
+    axios.get("https://api.vschool.io/rhettashoemaker/todo/")
+        .then(res => listData(res.data))
+        .catch(err => console.log(err))
+}
+
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
@@ -41,107 +88,8 @@ form.addEventListener("submit", function(event){
         completed: false,
     }
 
-    form.title.value = ""
-
-    axios.post("https://api.vschool.io/rhettashoemaker/todo", newItem)
-        .then(res => getData())
+    axios.post("https://api.vschool.io/rhettashoemaker/todo/", newItem)
+        .then(res => getData(res.data))
         .catch(err => console.log(err))
-
-    axios.post("https://api.vschool.io/rhettashoemaker/todo")
-        .then (response => {
-            const itemTitle = document.createElement('h5');
-            const itemPrice = document.createElement('h6'); 
-            const itemDescription = document.createElement('p');
-            const itemImageURL = document.createElement('p');
-            const itemEditButton = document.createElement('button');
-            const itemDeleteButton = document.createElement('button');
-            const itemCompleteButton = document.createElement('button');
-
-            itemContainer.appendChild(itemTitle);
-            itemContainer.appendChild(itemPrice);
-            itemContainer.appendChild(itemDescription);
-            itemContainer.appendChild(itemImageURL);
-            itemContainer.appendChild(itemEditButton);
-            itemContainer.appendChild(itemDeleteButton);
-            itemContainer.appendChild(itemCompleteButton);  
-        })
-        .catch(error => console.log(error))
 })
 
-
-
-//PUT//
-
-//complete//
-
-itemCompleteButton.addEventListener('click', function () {
-    
-
-
-    //     const updates = {
-//         title: "",
-//         description: ""
-//     }
-
-//     axios.put("https://api.vschool.io/scrimbalessons/todo/5d8bd531ee91575e6d49e06f", updates)
-//         .then(respo0nse => console.log(response.data))
-//         .catch(error => console.log(error))
-// }       
-
-    // itemEditButton.addEventListener('click', function () {
-            
-        // })
-        // itemDeleteButton.addEventListener('click', function () {
-        //     itemContainer.removeChild(itemContainer);
-        // })
-        // itemCompleteButton.addEventListener('click', function () {
-        //     itemTitle.style.textDecoration = "line-through";
-        //     itemTitle.style.color = "color:#86868F";
-        // })
-
-
-
-// function getData(){
-//     axios.get("https://api.vschool.io/rhettashoemaker/todo")
-//         .then(res => listData(res.data))
-//         .catch(err => alert(err))
-// }
-
-// function listData(data){
-//     clearOut()
-//     for(let i = 0; i < data.length; i++){
-//         const li = document.createElement('li')
-//         li.textContent = data[i].title
-//         document.getElementById('todoList').appendChild(li)
-//     }
-// }
-
-// function clearOut(){
-//     const el = document.getElementById('todoItemContainer')
-//     while(el.firstChild){
-//         el.removeChild(el.firstChild)
-//     }
-// }
-
-// getData()
-
-// form.addEventListener("submit", function(event){
-//     event.preventDefault()
-//     const newItem = {
-//         title: todoListForm.title.value
-//     }
-    
-//     todoListForm.title.value = ""
-    
-//     axios.post("", newItem)
-//         .then(res => getData(res.data))
-//         .catch(err => console.log(err))
-// })
-
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();
-//     const item = form['shopping-item'].value;
-//     addItem(item);
-
-//     return 'Finished';
-// })
