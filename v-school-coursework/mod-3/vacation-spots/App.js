6a1eb67f9a8c3c712d75f4e01c10329cdcd2a674
Rhetta Shoemaker <rhetta.shoemaker@gmail.com>
2020-04-15 23:05:55 -0600

    for heaven's sake why are there 5000

--- v-school-coursework/mod-3/vacation-spots/App.js
+++ v-school-coursework/mod-3/vacation-spots/App.js
@@ -5,7 +5,7 @@ import vacationSpotsData from "./vacationSpotsData"
 function App () {
     const spotComponents = vacationSpotsData.map(item => <Spot key = {item.place} spot = {item}/>)
     return (
-        <div className="wholeContainer">
+        <div className = "wholeContainer">
             {spotComponents}
         </div>
     )
